e3e8d587197ff4f1f14b96bcb724939b9be5e6bd
KeshavArvindh <150284351+KeshavArvindh@users.noreply.github.com>
2025-01-22 14:43:35 +0530

    Add files via upload

---
+++ frontend/src/services/transactions/transactionService.js
@@ -0,0 +1,83 @@
+import axios from "axios";
+import { BASE_URL } from "../../utils/url";
+import { getUserFromStorage } from "../../utils/getUserFromStorage";
+
+// Get the token
+const token = getUserFromStorage();
+
+//1. Add
+export const addTransactionAPI = async ({
+  type,
+  category,
+  date,
+  description,
+  amount,
+}) => {
+  const response = await axios.post(
+    `${BASE_URL}/transactions/create`,
+    {
+      category,
+      date,
+      description,
+      amount,
+      type,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  //Return a promise
+  return response.data;
+};
+
+//2. update
+export const updateCategoryAPI = async ({ name, type, id }) => {
+  const response = await axios.put(
+    `${BASE_URL}/categories/update/${id}`,
+    {
+      name,
+      type,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  //Return a promise
+  return response.data;
+};
+
+//3. delete
+export const deleteCategoryAPI = async (id) => {
+  const response = await axios.delete(`${BASE_URL}/categories/delete/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  //Return a promise
+  return response.data;
+};
+
+//4. lists
+export const listTransactionsAPI = async ({
+  category,
+  type,
+  startDate,
+  endDate,
+}) => {
+  const response = await axios.get(`${BASE_URL}/transactions/lists`, {
+    params: { category, endDate, startDate, type },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  //Return a promise
+  return response.data;
+};
